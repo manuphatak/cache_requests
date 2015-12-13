2b6f94a26946ac97b9dd5341df7a01a07e8383aa
Manu Phatak <bionikspoon@gmail.com>
2015-12-12 22:39:37 -0600

    Bump version: 2.0.0 → 2.0.1

--- cache_requests/__init__.py
+++ cache_requests/__init__.py
@@ -25,5 +25,5 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 __author__ = 'Manu Phatak'
 __email__ = 'bionikspoon@gmail.com'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __all__ = ['config', 'Session', 'Memoize']
