0b10815b665ddc07f5fbeaf57b47b2b746c9206a
Manu Phatak <bionikspoon@gmail.com>
2015-12-13 00:17:40 -0600

    fix dev status

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(  # :off
     cmdclass={'test': PyTest},
     keywords='cache_requests',
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
